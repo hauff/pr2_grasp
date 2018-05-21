34653bd69bcba74f990f543e4235bc26ca22bcb6
hauff <hauffn@informatik.uni-freiburg.de>
2018-05-21 17:00:58 +0200

    Modified: Output sensor position

--- ros_toolbox/src/transform_clouds_node.cpp
+++ ros_toolbox/src/transform_clouds_node.cpp
@@ -3,22 +3,46 @@
 #include <tf2_sensor_msgs/tf2_sensor_msgs.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/Vector3.h>
+
+const std::string ns = "ros_toolbox";
+const std::string name = "transform_clouds";
+
+bool compareVectors(const geometry_msgs::Vector3& vec1, const geometry_msgs::Vector3& vec2,
+  double epsilon = 1e-3)
+{
+  return
+    fabs(vec1.x - vec2.x) < epsilon &&
+    fabs(vec1.y - vec2.y) < epsilon &&
+    fabs(vec1.z - vec2.z) < epsilon;
+}
 
 void callback(const sensor_msgs::PointCloud2::ConstPtr& cloud_msg_ptr, tf2_ros::Buffer& tf_buffer,
-  const std::string& target_frame, ros::Publisher& pub_cloud)
+  const std::string& target_frame, geometry_msgs::Vector3& prev_sensor_position,
+  ros::Publisher& pub_cloud)
 {
   sensor_msgs::PointCloud2 cloud_msg;
+  geometry_msgs::Vector3 sensor_position;
 
   try
   {
-    geometry_msgs::TransformStamped transform = tf_buffer.lookupTransform(
+    geometry_msgs::TransformStamped ts = tf_buffer.lookupTransform(
       target_frame, cloud_msg_ptr->header.frame_id, cloud_msg_ptr->header.stamp);
-    tf2::doTransform(*cloud_msg_ptr, cloud_msg, transform);
+    tf2::doTransform(*cloud_msg_ptr, cloud_msg, ts);
+    sensor_position = ts.transform.translation;
   }
   catch (tf2::TransformException &ex)
   {
     ROS_WARN("%s", ex.what());
     return;
+  }
+
+  if (!compareVectors(prev_sensor_position, sensor_position))
+  {
+    ROS_INFO("[%s/%s]: Sensor position: %.3f, %.3f, %.3f", ns.c_str(), name.c_str(),
+     sensor_position.x, sensor_position.y, sensor_position.z);
+
+    prev_sensor_position = sensor_position;
   }
 
   pub_cloud.publish(cloud_msg);
@@ -56,11 +80,12 @@ int main(int argc, char** argv)
 
   tf2_ros::Buffer tf_buffer;
   tf2_ros::TransformListener tf_listener(tf_buffer);
+  geometry_msgs::Vector3 prev_sensor_position;
   ros::Publisher pub_cloud = nh_public.advertise<sensor_msgs::PointCloud2>(topic_out, 100);
 
   ros::Subscriber sub_cloud = nh_public.subscribe<sensor_msgs::PointCloud2>(topic_in, 100,
     boost::bind(callback, _1, boost::ref(tf_buffer), boost::ref(target_frame),
-    boost::ref(pub_cloud)));
+    boost::ref(prev_sensor_position), boost::ref(pub_cloud)));
 
   ros::spin();
 
